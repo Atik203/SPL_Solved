a3cf2e0a972c73633b31fef7224bc87357555f2e
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-06-30 21:03:20 +0600

    14 not solved

--- Problem_set_5/14.c
+++ Problem_set_5/14.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
-
-void swapArrays(int arr1[], int arr2[], int size) {
-    int i, temp;
-    for (i = 0; i < size; i++) {
-        temp = arr1[i];
-        arr1[i] = arr2[i];
-        arr2[i] = temp;
-    }
-}
-
 int main() {
     int n, m, i;
     scanf("%d", &n);
-    
     int arrayA[n];
-    
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+     {
         scanf("%d", &arrayA[i]);
     }
-    
     scanf("%d", &m);
-    
     int arrayB[m];
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < m; i++) 
+    {
         scanf("%d", &arrayB[i]);
     }
-    
-    swapArrays(arrayA, arrayB, (n < m) ? n : m);
-    
+
+    int temp[n];
+    for (int i = 0; i < n; i++) 
+    {
+        temp[i] = arrayA[i];
+    }
+
+    for (int i = 0; i < m; i++) 
+    {
+        arrayA[i] = arrayB[i];
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        arrayB[i] = temp[i];
+    }
     printf("Array A: ");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         printf("%d ", arrayA[i]);
     }
     
     printf("\nArray B: ");
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < m; i++) 
+    {
         printf("%d ", arrayB[i]);
     }
     
