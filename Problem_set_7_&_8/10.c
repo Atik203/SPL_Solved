28d190d7458ea8641002aa39686511841c303407
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-07-04 21:29:44 +0600

    .

--- Problem_set_7_&_8/10.c
+++ Problem_set_7_&_8/10.c
@@ -11,23 +11,26 @@ int main() {
         }
     }
 
-    // Calculate the sum for the specified positions
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-              if( (i == 0 && j < n/2) ||
-           (i == n-1 && j > n/2) ||
-           (j == (n/2) + 1)  || 
-           (j == n-1 && i < n/2) ||
-           (j == 0 && i > n/2) 
-         ) 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            {
+              if( (i == 0 &&( j <= n/2 || j==n-1)) ||  // first row
+                 (i == n-1 && (j >= n/2 || j==0)) ||    // last row
+                  (i==n/2) ||                              // middle row
+                  (((i>0 && i<n/2) && (j==n/2))) || 
+                   ((i>0 && i<n/2) && (j==n-1)) ||  // upper middle rows
+                 (((i<n-1 && i>n/2) && (j==n/2))) ||
+                 ((i>n/2 && i<n-1) && (j==n-1)))   // down middle rows
             {
                 sum += a[i][j];
             }
         }
     }
 
-    // Print the result
     printf("Total sum: %d\n", sum);
 
     return 0;
 }
+
+
